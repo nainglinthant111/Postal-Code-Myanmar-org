858b89054791a53b181eec3c208cc987cdf868c6
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-06-25 00:32:14 +0630

    fix(ui): update font

--- src/app/components/_nav.tsx
+++ src/app/components/_nav.tsx
@@ -31,8 +31,9 @@ const _nav = ({toggleDisplay}: displayProps) => {
         router.prefetch("/about");
     }, [router]);
     return (
-        <div className="flex justify-between fixed z-10 items-center border-b-2 p-4 px-4 sm:px-32 w-full bg-white">
-            <div className="cursor-pointer flex justify-center items-center" onClick={gotoHome}>
+        <div
+            className="flex justify-between fixed z-10 items-center border-b-2 p-4 px-4 sm:px-32 w-full bg-white">
+            <div className="cursor-pointer flex justify-center items-center font-poppin" onClick={gotoHome}>
                 <Image src={logo} alt="logo" className="w-8 h-8"/>
                 <span className="text-xl ml-1">M</span>
                 <span className="text-base pt-1">yanmar</span>
@@ -41,17 +42,17 @@ const _nav = ({toggleDisplay}: displayProps) => {
                 <span className="text-xl ml-1">C</span>
                 <span className="text-base pt-1">ode</span>
             </div>
-            <div className="gap-10 flex">
-                <div onClick={gotoHome} className="cursor-pointer hidden sm:block text-gray-500 font-mono text-lg">
+            <div className="gap-10 flex font-poppin">
+                <div onClick={gotoHome} className="cursor-pointer hidden sm:block text-gray-600 text-lg">
                     Home
                 </div>
-                <div onClick={gotoState} className="cursor-pointer hidden sm:block text-gray-500 font-mono text-lg">
+                <div onClick={gotoState} className="cursor-pointer hidden sm:block text-gray-600 text-lg">
                     Demo
                 </div>
-                <div onClick={gotoService} className="cursor-pointer hidden sm:block text-gray-500 font-mono text-lg">
+                <div onClick={gotoService} className="cursor-pointer hidden sm:block text-gray-600 text-lg">
                     Docs
                 </div>
-                <div onClick={gotoAbout} className="cursor-pointer hidden sm:block text-gray-500 font-mono text-lg">
+                <div onClick={gotoAbout} className="cursor-pointer hidden sm:block text-gray-600 text-lg">
                     About
                 </div>
                 <div className="sm:hidden" onClick={toggleDisplay}>
