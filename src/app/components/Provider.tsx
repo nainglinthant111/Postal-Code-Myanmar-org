e74e2d0020492efc8bbf7a0d7de25c5bdaeef334
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-06-25 00:33:21 +0630

    fix(ui): remove footer

--- src/app/components/Provider.tsx
+++ src/app/components/Provider.tsx
@@ -3,7 +3,6 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {PropsWithChildren, useState} from "react";
 import Menu from "@/app/components/_menu";
 import Nav from "@/app/components/_nav";
-import Footer from "@/app/components/_footer";
 
 const Providers = ({children}: PropsWithChildren<{}>) => {
     const [display, setDisplay] = useState(false);
@@ -27,7 +26,6 @@ const Providers = ({children}: PropsWithChildren<{}>) => {
                 <div>
                     {children}
                 </div>
-                <Footer/>
             </>
         )}
     </QueryClientProvider>
