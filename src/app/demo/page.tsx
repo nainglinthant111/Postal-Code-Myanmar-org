1f2e8dd5640a44016ef621eaa3b0ba8a1b609d3d
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-07-01 22:34:37 +0630

    fix(ui): mobile view

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -74,94 +74,98 @@ const page = () => {
             {isSuccess && (
                 <div>
                     <div className="container sm:mx-auto pt-20">
-                        <div className="flex justify-end gap-4">
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button variant="outline">
-                                        Filters
-                                        <div className="pt-1 pl-1">
-                                            <ChevronDown/>
-                                        </div>
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent className="w-56">
-                                    <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                        <DropdownMenuRadioItem
-                                            value="Quarters"
-                                            onClick={() => handleFilterQuarters()}>
-                                            Quarters
-                                        </DropdownMenuRadioItem>
-                                        <DropdownMenuRadioItem
-                                            value="Township"
-                                            onClick={() => handleFilterTownship()}>
-                                            Township
-                                        </DropdownMenuRadioItem>
-                                        <DropdownMenuRadioItem
-                                            value="PostalCode"
-                                            onClick={() => handleFilterPostalCode()}>
-                                            PostalCode
-                                        </DropdownMenuRadioItem>
-                                    </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button variant="outline">
-                                        {allLoading ? (
-                                            <span className="loading loading-spinner loading-xs"></span>
-                                        ) : (
-                                            <>
-                                                {regionValue}
-                                                <div className="pt-1 pl-1">
-                                                    <ChevronDown/>
-                                                </div>
-                                            </>
-                                        )}
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                {allSuccess && (
-                                    <DropdownMenuContent className="w-56 h-[310px]">
-                                        <ScrollArea className="h-[300px] w-56 rounded-md">
-                                            <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                                {regionList?.map((item: any, index: number) => (
-                                                    <div key={index}>
-                                                        <DropdownMenuRadioItem
-                                                            value={item.region_code}
-                                                            onClick={() => handleRegionCodeSelection(item.region_code, item.region)}>
-                                                            {item.region}
-                                                        </DropdownMenuRadioItem>
-                                                        {index < regionList.length - 1 && <DropdownMenuSeparator/>}
+                        <div className="flex flex-col md:flex-row justify-end gap-5">
+                            <div>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button variant="outline">
+                                            {allLoading ? (
+                                                <span className="loading loading-spinner loading-xs"></span>
+                                            ) : (
+                                                <>
+                                                    {regionValue}
+                                                    <div className="pt-1 pl-1">
+                                                        <ChevronDown/>
                                                     </div>
-                                                ))}
-                                            </DropdownMenuRadioGroup>
-                                        </ScrollArea>
+                                                </>
+                                            )}
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    {allSuccess && (
+                                        <DropdownMenuContent className="w-56 h-[310px]">
+                                            <ScrollArea className="h-[300px] w-56 rounded-md">
+                                                <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+                                                    {regionList?.map((item: any, index: number) => (
+                                                        <div key={index}>
+                                                            <DropdownMenuRadioItem
+                                                                value={item.region_code}
+                                                                onClick={() => handleRegionCodeSelection(item.region_code, item.region)}>
+                                                                {item.region}
+                                                            </DropdownMenuRadioItem>
+                                                            {index < regionList.length - 1 && <DropdownMenuSeparator/>}
+                                                        </div>
+                                                    ))}
+                                                </DropdownMenuRadioGroup>
+                                            </ScrollArea>
+                                        </DropdownMenuContent>
+                                    )}
+                                </DropdownMenu>
+                            </div>
+                            <div className="flex gap-5">
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button variant="outline">
+                                            {langData}
+                                            <div className="pt-1 pl-1">
+                                                <ChevronDown/>
+                                            </div>
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent className="w-56">
+                                        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+                                            <DropdownMenuRadioItem
+                                                value="English"
+                                                onClick={() => handleEnglish()}>
+                                                English
+                                            </DropdownMenuRadioItem>
+                                            <DropdownMenuRadioItem
+                                                value="Myanmar"
+                                                onClick={() => handleMyanmar()}>
+                                                Myanmar
+                                            </DropdownMenuRadioItem>
+                                        </DropdownMenuRadioGroup>
                                     </DropdownMenuContent>
-                                )}
-                            </DropdownMenu>
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button variant="outline">
-                                        {langData}
-                                        <div className="pt-1 pl-1">
-                                            <ChevronDown/>
-                                        </div>
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent className="w-56">
-                                    <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                        <DropdownMenuRadioItem
-                                            value="English"
-                                            onClick={() => handleEnglish()}>
-                                            English
-                                        </DropdownMenuRadioItem>
-                                        <DropdownMenuRadioItem
-                                            value="Myanmar"
-                                            onClick={() => handleMyanmar()}>
-                                            Myanmar
-                                        </DropdownMenuRadioItem>
-                                    </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
+                                </DropdownMenu>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button variant="outline">
+                                            Filters
+                                            <div className="pt-1 pl-1">
+                                                <ChevronDown/>
+                                            </div>
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent className="w-56">
+                                        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+                                            <DropdownMenuRadioItem
+                                                value="Quarters"
+                                                onClick={() => handleFilterQuarters()}>
+                                                Quarters
+                                            </DropdownMenuRadioItem>
+                                            <DropdownMenuRadioItem
+                                                value="Township"
+                                                onClick={() => handleFilterTownship()}>
+                                                Township
+                                            </DropdownMenuRadioItem>
+                                            <DropdownMenuRadioItem
+                                                value="PostalCode"
+                                                onClick={() => handleFilterPostalCode()}>
+                                                PostalCode
+                                            </DropdownMenuRadioItem>
+                                        </DropdownMenuRadioGroup>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div>
                         </div>
                         {langData === "Myanmar" ? (
                             <MyanDataTable columns={myanColumns} data={singleRegion.data} columnsValue={filter}/>
