2d85cc12aa5d5d344bccb0af150d3f2ed3524d49
MinThantMaung <minthant180@gmail.com>
2024-10-07 11:48:45 +0900

    fix:update ui

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 import React from "react";
-import {Box} from "lucide-react";
+import { Box } from "lucide-react";
 import Endpoint from "../../app/components/_endpoint"
 import Usage from "../../app/components/_usage"
-// @ts-ignore
 import detail from "../../../public/detail.png"
 import Footer from "../components/_footer"
 import Image from "next/image";
@@ -11,28 +10,61 @@ import Image from "next/image";
 const page = () => {
 
     return (
-        <div className="w-full">
-            <div className="sm:px-36 px-8 pr-14 sm:pr-0 grid gap-y-10">
-                <div className="sm:pt-28 pt-20">
-                    <div className="font-bold text-2xl text-yellow-600">
-                        How to use it?
-                    </div>
-                    <div className="text-gray-400 sm:pt-8 sm:mr-10">
-                        Myanmar Postal Code API can be used with any type of project that needs postal code and township
-                        of myanmar in JSON format. you can use examples below to check how Myanmar Postal Code API works
-                        and feel free to enjoy it in your awesome projects!
-                    </div>
+        // <div className="flex items-center ">
+        //     <div className="max-w-6xl">
+        //         <div className="grid gap-y-10">
+        //             <div className="sm:pt-28 pt-20">
+        //                 <div className="font-bold text-2xl text-yellow-600">
+        //                     How to use it?
+        //                 </div>
+        //                 <div className="text-gray-400 sm:pt-8 sm:mr-10">
+        //                     Myanmar Postal Code API can be used with any type of project that needs postal code and township
+        //                     of myanmar in JSON format. you can use examples below to check how Myanmar Postal Code API works
+        //                     and feel free to enjoy it in your awesome projects!
+        //                 </div>
+        //             </div>
+        //             {/*<div>
+        //             <div className="font-bold text-2xl text-yellow-600">
+        //                 How postal code work
+        //             </div>
+        //             <div className="pt-6">
+        //                 <Image src={detail} alt="detail_ing" />
+        //             </div>
+        //         </div>
+        //         <div className="font-bold text-2xl flex items-center gap-2">
+        //             <Box/>
+        //             <div className="text-yellow-600">
+        //                 States and Postal Codes
+        //             </div>
+        //         </div>
+        //         <Endpoint />
+        //         <Usage /> */}
+        //         </div>
+        //         <Footer />
+        //     </div>
+        // </div>
+        <div className="container mx-auto px-4 pt-8">
+            <div className="max-w-5xl mx-auto sm:mt-14 mt-14">
+                <div className="font-bold text-2xl text-yellow-600">
+                    How to use it?
                 </div>
-                <div>
-                    <div className="font-bold text-2xl text-yellow-600">
-                        How postal code work
-                    </div>
-                    <div className="pt-6">
-                        <Image src={detail} alt="detail_ing" />
-                    </div>
+                <div className="text-gray-400 sm:pt-8 pt-6 sm:mr-10">
+                    Myanmar Postal Code API can be used with any type of project that needs postal code and township
+                    of Myanmar in JSON format. You can use examples below to check how Myanmar Postal Code API works
+                    and feel free to enjoy it in your awesome projects!
                 </div>
-                <div className="font-bold text-2xl flex items-center gap-2">
-                    <Box/>
+                <div className="font-bold text-2xl text-yellow-600 sm:mt-10 mt-6">
+                    How postal code work
+                </div>
+                <div className="pt-6">
+                    <Image
+                        src={detail}
+                        alt="detail_ing"
+                        className="w-full w-[500px] sm:w-[600px]"
+                    />
+                </div>
+                <div className="font-bold text-2xl flex items-center gap-2 mt-6">
+                    <Box />
                     <div className="text-yellow-600">
                         States and Postal Codes
                     </div>
@@ -40,7 +72,7 @@ const page = () => {
                 <Endpoint />
                 <Usage />
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
