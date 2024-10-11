c2a5f15046e653cb9a0d335c69ca1819b8c1a275
MinThantMaung <minthant180@gmail.com>
2024-10-11 10:34:24 +0900

    fix:add login btn in nav

--- src/app/components/_nav.tsx
+++ src/app/components/_nav.tsx
@@ -4,6 +4,7 @@ import {useRouter} from "next/navigation";
 import {displayProps} from "../types";
 import Image from "next/image";
 import logo from "../../../public/myanmar.png"
+import { Button } from "@/components/ui/button";
 
 const _nav = ({toggleDisplay}: displayProps) => {
     const router = useRouter();
@@ -42,21 +43,24 @@ const _nav = ({toggleDisplay}: displayProps) => {
                 <span className="text-xl ml-1">C</span>
                 <span className="text-base pt-1">ode</span>
             </div>
-            <div className="gap-10 flex">
+            <div className="flex justify-center items-center">
                 <div onClick={gotoHome} className="cursor-pointer hidden sm:block text-gray-600 text-lg">
                     Home
                 </div>
-                <div onClick={gotoState} className="cursor-pointer hidden sm:block text-gray-600 text-lg">
+                <div onClick={gotoState} className="cursor-pointer hidden sm:block text-gray-600 text-lg mx-5">
                     Demo
                 </div>
-                <div onClick={gotoService} className="cursor-pointer hidden sm:block text-gray-600 text-lg">
+                <div onClick={gotoService} className="cursor-pointer hidden sm:block text-gray-600 text-lg mr-5">
                     Docs
                 </div>
-                <div onClick={gotoAbout} className="cursor-pointer hidden sm:block text-gray-600 text-lg">
+                <div onClick={gotoAbout} className="cursor-pointer hidden sm:block text-gray-600 text-lg mr-5">
                     About
                 </div>
                 <div className="sm:hidden" onClick={toggleDisplay}>
                     <Menu/>
+                </div>
+                <div className="hidden sm:block mr-5">
+                    <button className="px-4 py-1.5 border-2 border-blue-500 rounded-lg text-blue-500 text-lg">Login</button>
                 </div>
             </div>
         </div>
