5c554fc8aaf1302628f488b1e71da6e55d3eecf1
Min Thant Mg <minthant180@gmail.com>
2024-10-18 00:39:56 +0900

    feat:add route to profile

--- src/app/components/_nav.tsx
+++ src/app/components/_nav.tsx
@@ -1,75 +1,108 @@
-import React, {useEffect} from "react";
-import {Menu} from "lucide-react";
-import {useRouter} from "next/navigation";
-import {displayProps} from "../types";
+import React, { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { displayProps } from "../types";
 import Image from "next/image";
-import logo from "../../../public/myanmar.png"
+import logo from "../../../public/myanmar.png";
 import { Button } from "@/components/ui/button";
 
-const _nav = ({toggleDisplay}: displayProps) => {
-    const router = useRouter();
+const _nav = ({ toggleDisplay }: displayProps) => {
+  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
 
-    const gotoHome = () => {
-        router.push("/");
-    };
+  const gotoHome = () => {
+    router.push("/");
+  };
 
-    const gotoState = () => {
-        router.push("/demo");
-    };
+  const gotoState = () => {
+    router.push("/demo");
+  };
 
-    const gotoService = () => {
-        router.push("/docs");
-    };
+  const gotoService = () => {
+    router.push("/docs");
+  };
 
-    const gotoAbout = () => {
-        router.push("/about");
-    };
+  const gotoAbout = () => {
+    router.push("/about");
+  };
 
-    const gotoLogin = () => {
-        router.push("/auth/sign-in")
-    }
+  const gotoLogin = () => {
+    router.push("/auth/sign-in");
+  };
 
-    useEffect(() => {
-        router.prefetch("/");
-        router.prefetch("/demo");
-        router.prefetch("/docs");
-        router.prefetch("/about");
-        router.prefetch("/auth/sign-in");
-    }, [router]);
-    return (
+  const navigateToProfile = () => {
+    router.push("/profile")
+  }
+
+  useEffect(() => {
+    router.prefetch("/");
+    router.prefetch("/demo");
+    router.prefetch("/docs");
+    router.prefetch("/about");
+    router.prefetch("/auth/sign-in");
+    router.prefetch("/profile")
+  }, [router]);
+  return (
+    <div className="flex justify-between fixed z-10 items-center border-b-2 p-4 px-4 sm:px-32 w-full bg-white">
+      <div
+        className="cursor-pointer flex justify-center items-center"
+        onClick={gotoHome}
+      >
+        <Image src={logo} alt="logo" className="w-8 h-8" />
+        <span className="text-xl ml-1">M</span>
+        <span className="text-base pt-1">yanmar</span>
+        <span className="text-xl ml-1">P</span>
+        <span className="text-base pt-1">ostal</span>
+        <span className="text-xl ml-1">C</span>
+        <span className="text-base pt-1">ode</span>
+      </div>
+      <div className="flex justify-center items-center">
         <div
-            className="flex justify-between fixed z-10 items-center border-b-2 p-4 px-4 sm:px-32 w-full bg-white">
-            <div className="cursor-pointer flex justify-center items-center" onClick={gotoHome}>
-                <Image src={logo} alt="logo" className="w-8 h-8"/>
-                <span className="text-xl ml-1">M</span>
-                <span className="text-base pt-1">yanmar</span>
-                <span className="text-xl ml-1">P</span>
-                <span className="text-base pt-1">ostal</span>
-                <span className="text-xl ml-1">C</span>
-                <span className="text-base pt-1">ode</span>
-            </div>
-            <div className="flex justify-center items-center">
-                <div onClick={gotoHome} className="cursor-pointer hidden sm:block text-gray-600 text-lg">
-                    Home
-                </div>
-                <div onClick={gotoState} className="cursor-pointer hidden sm:block text-gray-600 text-lg mx-5">
-                    Demo
-                </div>
-                <div onClick={gotoService} className="cursor-pointer hidden sm:block text-gray-600 text-lg mr-5">
-                    Docs
-                </div>
-                <div onClick={gotoAbout} className="cursor-pointer hidden sm:block text-gray-600 text-lg mr-5">
-                    About
-                </div>
-                <div className="sm:hidden" onClick={toggleDisplay}>
-                    <Menu/>
-                </div>
-                <div className="cursor-pointer hidden sm:block text-gray-600 text-lg mr-5">
-                     Profile
-                </div>
-            </div>
+          onClick={gotoHome}
+          className="cursor-pointer hidden sm:block text-gray-600 text-lg"
+        >
+          Home
         </div>
-    );
+        <div
+          onClick={gotoState}
+          className="cursor-pointer hidden sm:block text-gray-600 text-lg mx-5"
+        >
+          Demo
+        </div>
+        <div
+          onClick={gotoService}
+          className="cursor-pointer hidden sm:block text-gray-600 text-lg mr-5"
+        >
+          Docs
+        </div>
+        <div
+          onClick={gotoAbout}
+          className="cursor-pointer hidden sm:block text-gray-600 text-lg mr-5"
+        >
+          About
+        </div>
+        <div className="sm:hidden" onClick={toggleDisplay}>
+          <Menu />
+        </div>
+        {isAuthenticated ? (
+          <div className="hidden sm:block mr-5">
+            <div onClick={navigateToProfile} className="cursor-pointer hidden sm:block text-gray-600 text-lg mr-5">
+              Profile
+            </div>
+          </div>
+        ) : (
+          <div className="hidden sm:block mr-5">
+            <button
+              className="px-4 py-1.5 border-2 border-blue-500 rounded-lg text-blue-500 text-lg cursor-pointer"
+              onClick={gotoLogin}
+            >
+              Login
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default _nav;
