7be2ece261aceeff0f5b615f2eac3365f6cc260d
MinThant <minthant180@gmail.com>
2024-10-21 17:46:18 +0900

    fix:handle responsive

--- src/app/auth/forget-password/page.tsx
+++ src/app/auth/forget-password/page.tsx
@@ -20,21 +20,21 @@ const Page = () => {
       <div className="text-sm text-gray-500 mb-8">
         No worries, we'll send you reset instructions.
       </div>
-      <div className="md:w-80">
+      <div className="w-80">
         <div>Email</div>
         <Input
           type="email"
           placeholder="Email"
-          className="md:mt-2 border md:py-1 md:px-3 rounded-sm w-full"
+          className="mt-2 border py-1 md:px-3 rounded-sm w-full"
         />
       </div>
-      <Button className="w-full max-w-xs md:mt-6" onClick={gotoReset}>
+      <Button className="w-full max-w-xs mt-6" onClick={gotoReset}>
         Reset password
       </Button>
       <Link href="/auth/sign-in">
-        <div className="flex md:mt-6 cursor-pointer">
+        <div className="flex mt-6 cursor-pointer">
           <MoveLeft />
-          <span className="md:ml-2">Back to log in</span>
+          <span className="ml-2">Back to log in</span>
         </div>
       </Link>
     </div>
